d1af40313f0fd29326a81b8c090c1e6b9ffd7d19
Arpit Mishra <39494066+mishrraG@users.noreply.github.com>
2020-04-22 18:07:27 +0530

    Updates

--- main.c
+++ main.c
@@ -1,39 +1,43 @@
+/**
+This program is a representation of a Vertical Histogram
+Author : Arpit Kumar Mishra
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 void main()
 {
-    char a[100];
-    printf("Enter the sentence \n");
-    gets(a);
+    char a[100]="SANSKRITI Vijay";
     int l=strlen(a);
-    int c[26];
+    int freq[26];       // array to store the frequency of each letter in the string a
     for(int i=0;i<26;i++)
     {
-        c[i]=0;
+        freq[i]=0;
     }
-    for(char i='A';i<='Z';i++)
+    for(char i='A';i<='Z' ;i++)
     {
         for(int j=0;j<l;j++)
         {
-            if(a[j]==i)
+            if(a[j]==i || a[j]==i+32)     //the frequency is stored for both uppercase and lowercase letters
             {
-                c[i-'A']++;
+                freq[i-'A']++;
             }
         }
     }
-    int max=c[0];
-    for(int i=0;i<26;i++)
-    {
-        max=(max<c[i])?c[i]:max;
+    int max=freq[0];
+    for(int i=0;i<26;i++)         // Calculates the maximum frequency in the frequency array as it will be required to
+    {                             // y=triangulate the height of the histogram
+        max=(max<freq[i])?freq[i]:max;
     }
-    for(int i=1;i<=max;i++)
+    printf("The sentence in consideration is - %s\n",a);
+    for(int i=1;i<=max;i++)       // Main loop responsible for printing the histogram the way it's printed
     {
         for(int j=0;j<26;j++)
         {
-            if(c[j]==(max-i+1))
+            if(freq[j]==(max-i+1))
             {
                 printf("*");
-                c[j]--;
+                freq[j]--;
             }
             else
             {
@@ -42,7 +46,13 @@ void main()
         }
         printf("\n");
     }
-    for(char q='A';q<='Z';q++)
+
+    for(char q='A';q<='Z';q++)     // This is responsible for printing the x-axis i.e. the base of the histogram
+    {
+        printf("%c",q);
+    }
+    printf("\n");
+    for(char q='a';q<='z';q++)     // This is responsible for printing the x-axis i.e. the base of the histogram
     {
         printf("%c",q);
     }
